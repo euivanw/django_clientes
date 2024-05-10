f5cff0dba74aa5a0fe762b835b1a6ca1a7c799ef
Ivan Wilhelm <contato@ivanwilhelm.dev>
2024-05-10 12:00:39 -0300

    feat: Inclusão de validação para os campos nome, rg e celular.

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -11,5 +11,21 @@ class ClienteSerializer(serializers.ModelSerializer):
     def validate_cpf(self, cpf):
         if len(cpf) != 11:
             raise serializers.ValidationError('O CPF deve conter 11 dígitos.')
-
         return cpf
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome não deve incluir números.')
+        return nome
+
+    def validate_rg(self, rg):
+        if len(rg) != 11:
+            raise serializers.ValidationError('O RG deve conter 11 dígitos.')
+        return rg
+
+    def validate_celular(self, celular):
+        if not celular.isdigit():
+            raise serializers.ValidationError('O celular só pode conter números.')
+        if len(celular) != 11:
+            raise serializers.ValidationError('O celular deve conter 11 dígitos.')
+        return celular
