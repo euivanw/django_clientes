48dd442566968ecb152dcd5ffbf9cd83f13a2dd0
Ivan Wilhelm <contato@ivanwilhelm.dev>
2024-05-10 11:30:04 -0300

    prume: Estrutura inicial do projeto.

---
+++ clientes/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+from clientes.models import Cliente
+from clientes.serializers import ClienteSerializer
+
+
+class ClientesViewSet(viewsets.ModelViewSet):
+    """Listando clientes"""
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
